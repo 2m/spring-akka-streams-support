d5d37b4c37879f0fe4c43ee40bb1d1471cfe45f1
Martynas Mickevičius <self@2m.lt>
2017-11-20 10:56:51 +0200

    Add a test app

--- build.sbt
+++ build.sbt
@@ -3,11 +3,16 @@ organization := "com.lightbend.akka"
 
 version := "0.1"
 
-scalaVersion := "2.12.3"
+resolvers += "Staging Repo" at "https://oss.sonatype.org/content/repositories/comtypesafe-2013"
+scalaVersion := "2.12.4"
         
-val akkaVersion = "2.5.5"
+val akkaVersion = "2.5.7"
 val springVersion = "5.0.0.RELEASE"
 
 libraryDependencies += "org.springframework" % "spring-core" % springVersion
 libraryDependencies += "org.springframework" % "spring-context" % springVersion
+libraryDependencies += "org.springframework" % "spring-webmvc" % springVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % akkaVersion
+
+libraryDependencies += "org.springframework.boot" % "spring-boot" % "1.5.8.RELEASE" % Test
+libraryDependencies += "org.springframework.boot" % "spring-boot-autoconfigure" % "1.5.8.RELEASE" % Test
--- src/main/java/akka/stream/spring/adapter/AkkaStreamsCompatConfiguration.java
+++ src/main/java/akka/stream/spring/adapter/AkkaStreamsCompatConfiguration.java
@@ -2,16 +2,20 @@ package akka.stream.spring.adapter;
 
 import akka.actor.ActorSystem;
 import akka.event.Logging;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ReactiveAdapterRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
 
 @Configuration
 public class AkkaStreamsCompatConfiguration {
 
   @Autowired
-  public RegisterAkkaStreams(RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
+  public AkkaStreamsCompatConfiguration(RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
     final ReactiveAdapterRegistry registry = requestMappingHandlerAdapter.getReactiveAdapterRegistry();
 
-    final ActorSystem system = ActorSystem.create(Logging.simpleName(DemoApplication.class));
+    final ActorSystem system = ActorSystem.create(Logging.simpleName(AkkaStreamsCompatConfiguration.class));
     new AkkaStreamsRegistrar(system).registerAdapters(registry);
   }
   
